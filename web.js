0f2e87f77d25109df90bc674e2894ae02eb0bbe0
sgriffiths <sgriffiths1975@gmail.com>
2013-07-08 23:37:46 +0000

    web.js2

--- web.js
+++ web.js
@@ -4,14 +4,11 @@ var fs = require('fs')
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-
-   response.send(fs.readFile('/index.html').tostring());
-    //response.send('Hello World! 2);                                              
-
+  response.send(fs.readFileSync('index.html').toString());
+  //response.send('Hello World! 2');
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-
